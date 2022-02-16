e69820042805a5077503a00e1c346a291ae342a1
Juan-luke Klopper <jlklopper@protonmail.com>
2022-02-16 15:01:27 +0200

    Updated NFT contract

--- contracts/LukesEpicNFT.sol
+++ contracts/LukesEpicNFT.sol
@@ -1,10 +1,29 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "hardhat/console.sol";
 
-contract LukesEpicNFT {
-    constructor() {
+contract LukesEpicNFT is ERC721URIStorage {
+
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    constructor() ERC721 ("LukesFirstNFT","LFNFT"){
         console.log("This is my first NFT contract!");
+    }
+
+    function makeAnEpicNFT() public {
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        _setTokenURI(newItemId, "https://jsonkeeper.com/b/XF57");
+
+        console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+
+    // Increment the counter for when the next NFT is minted.
+        _tokenIds.increment();
     }
 }
