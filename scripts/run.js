b330059db3cebf401eacc3472e3dcfff8f08051a
Juan-luke Klopper <jlklopper@protonmail.com>
2022-02-16 15:22:05 +0200

    Updated run.js to mint 2 NFTs locally

--- scripts/run.js
+++ scripts/run.js
@@ -3,6 +3,12 @@ const main = async () => {
     const nftContract = await nftContractFactory.deploy();
     await nftContract.deployed();
     console.log("Contract deployed to:", nftContract.address);
+
+    let txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
+
+    let txn = await nftContract.makeAnEpicNFT();
+    await txn.wait();
 };
   
 const runMain = async () => {
